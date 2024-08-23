f430fef4a9902cba1b55616c2d195ad241fc95b4
ysmuei <108169494+ysmuei@users.noreply.github.com>
2024-08-23 14:02:52 +0900

    view engine push

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
